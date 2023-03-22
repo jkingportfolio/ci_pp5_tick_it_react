14dc90e5dd77f31bd0614a225057a4fa93a2507b
jkingportfolio <mycodejourney3@gmail.com>
2023-03-22 21:34:44 +0000

    Add: axiosDefaults.js and link to Heroku

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import {Routes, Route} from "react-router-dom";
+import './api/axiosDefaults';
 
 function App() {
   return (
@@ -12,7 +13,6 @@ function App() {
           <Route exact path="/" element={<h1>Home page</h1>} />
           <Route exact path="/signin" element={<h1>Sign in</h1>} />
           <Route exact path="/signup" element={<h1>Sign up</h1>} />
-          <Route element={<p>Page not found!</p>} />
         </Routes>
       </Container>
     </div>
