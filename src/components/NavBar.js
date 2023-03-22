392b7f9558a56d86d7a75d36007494dd69246691
jkingportfolio <mycodejourney3@gmail.com>
2023-03-22 20:39:57 +0000

    Add: Basic routes to nav bar links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,33 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-// import NavDropdown from "react-bootstrap/NavDropdown";
+import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar expand="lg" fixed="top" className={styles.NavBar}>
       <Container>
-        <Navbar.Brand>
-          <img src={logo} alt="logo" height="45" />
-        </Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="45" />
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link>
+            <NavLink to="/">
               <i className="fa-solid fa-house" />
               Home
-            </Nav.Link>
-            <Nav.Link>
-              <i class="fa-solid fa-right-to-bracket" />
+            </NavLink>
+            <NavLink to="/signin">
+              <i className="fa-solid fa-right-to-bracket" />
               Sign in
-            </Nav.Link>
-            <Nav.Link>
-              <i class="fa-solid fa-user-plus" />
+            </NavLink>
+            <NavLink to="/signup">
+              <i className="fa-solid fa-user-plus" />
               Sign up
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
