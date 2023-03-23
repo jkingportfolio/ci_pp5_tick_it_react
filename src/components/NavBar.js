329926226fb0922b46bb920db7bdb206a52186a1
jkingportfolio <mycodejourney3@gmail.com>
2023-03-23 23:30:17 +0000

    Refact: NavBar.js file with currentuser

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,12 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { CurrentUserContext } from "../App";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
 
 const NavBar = () => {
-  const currentUser = useContext(CurrentUserContext);
+  const currentUser = useCurrentUser();
   const loggedInIcons = <>{currentUser?.username}</>;
   const loggedOutIcons = (
     <>
@@ -36,7 +36,7 @@ const NavBar = () => {
         </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto text-right">
+          <Nav className="ml-auto text-left">
             <NavLink
               exact
               className={styles.NavLink}
