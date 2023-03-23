db1eb84dddd45d8c54bb4fd9e39bebe8cc37cf00
jkingportfolio <mycodejourney3@gmail.com>
2023-03-23 13:12:58 +0000

    Add: Basic sign up page & form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import {Routes, Route} from "react-router-dom";
 import './api/axiosDefaults';
+import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<h1>Home page</h1>} />
           <Route exact path="/signin" element={<h1>Sign in</h1>} />
-          <Route exact path="/signup" element={<h1>Sign up</h1>} />
+          <Route exact path="/signup" element={<SignUpForm />} />
         </Routes>
       </Container>
     </div>
