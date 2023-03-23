c3b63e66565b0371fbea7e6810a903c9cea916a6
jkingportfolio <mycodejourney3@gmail.com>
2023-03-23 21:57:50 +0000

    Add: Set current user context

--- src/pages/auth/LogInForm.js
+++ src/pages/auth/LogInForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import axios from "axios";
 
 import { Alert, Form, Button, Col, Row, Container } from "react-bootstrap";
@@ -8,8 +8,11 @@ import { Link, useHistory } from "react-router-dom";
 import styles from "../../styles/LogInForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
+import { SetCurrentUserContext } from "../../App";
 
 function SignInForm() {
+  const setCurrentUser = useContext(SetCurrentUserContext);
+
   const [signInData, setSignInData] = useState({
     username: "",
     password: "",
@@ -22,7 +25,8 @@ function SignInForm() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      await axios.post("/dj-rest-auth/login/", signInData);
+      const { data } = await axios.post("/dj-rest-auth/login/", signInData);
+      setCurrentUser(data.user);
       history.push("/");
     } catch (err) {
       setErrors(err.response?.data);
