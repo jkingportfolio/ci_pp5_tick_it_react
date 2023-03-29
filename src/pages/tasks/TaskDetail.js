21735670b4ac01f2003af21d481e9e4a7d05cc17
jkingportfolio <mycodejourney3@gmail.com>
2023-03-29 11:54:24 +0000

    Add: Hook to get api info by task id

--- src/pages/tasks/TaskDetail.js
+++ src/pages/tasks/TaskDetail.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Col, Row, Container } from "react-bootstrap";
 
+import { useParams } from "react-router";
+
 import appStyles from "../../App.module.css";
 
-function TaskDetail() {
+import { axiosReq } from "../../api/axiosDefaults";
 
+function TaskDetail() {
+  const { id } = useParams();
+  const [task, setTask] = useState({ results: [] });
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: task }] = await Promise.all([
+          axiosReq.get(`/tasks/${id}`),
+        ]);
+        setTask({ results: [task] });
+        console.log(task);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [id]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Task Detail</p>
-        <Container className={appStyles.Content}>
-          Body of Task
-        </Container>
+        <Container className={appStyles.Content}>Body of Task</Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
         Side bar to display other information
