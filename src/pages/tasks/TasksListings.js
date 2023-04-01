e7161dff919389314c0c38d9a84a1baf749ba6d3
jkingportfolio <mycodejourney3@gmail.com>
2023-04-01 09:59:48 +0000

    Add: Basis of search functionality

--- src/pages/tasks/TasksListings.js
+++ src/pages/tasks/TasksListings.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row, Form } from "react-bootstrap";
 
 import appStyles from "../../App.module.css";
+import styles from "../../styles/SearchBar.module.css";
 // import styles from "../../styles/TasksListings.module.css";
 
 import Task from "./Task";
@@ -13,54 +14,65 @@ import { axiosReq } from "../../api/axiosDefaults";
 
 import NoResults from "../../assets/no-results.png";
 
-
 function TasksListings({ message, filter = "" }) {
-    const [tasks, setTasks] = useState({ results: [] });
-    const [hasLoaded, setHasLoaded] = useState(false);
-    const { pathname } = useLocation();
-  
-    useEffect(() => {
-      const fetchTasks = async () => {
-        try {
-          const { data } = await axiosReq.get(`/tasks/?${filter}`);
-          setTasks(data);
-          setHasLoaded(true);
-        } catch (err) {
-          console.log(err);
-        }
-      };
-  
-      setHasLoaded(false);
-      fetchTasks();
-    }, [filter, pathname]);
-  
-    return (
-      <Row className="h-100">
-        <Col className="py-2 p-0 p-lg-2" lg={8}>
-          <p>Popular profiles mobile</p>
-          {hasLoaded ? (
-            <>
-              {tasks.results.length ? (
-                tasks.results.map((tasks) => (
-                  <Task key={tasks.id} {...tasks} setTasks={setTasks} />
-                ))
-              ) : (
-                <Container className={appStyles.Content}>
-                  <Asset src={NoResults} message={message} />
-                </Container>
-              )}
-            </>
-          ) : (
-            <Container className={appStyles.Content}>
-              <Asset spinner />
-            </Container>
-          )}
-        </Col>
-        <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-          <p>Popular profiles for desktop</p>
-        </Col>
-      </Row>
-    );
-  }
-  
-  export default TasksListings;
+  const [tasks, setTasks] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const { data } = await axiosReq.get(`/tasks/?${filter}search=${query}`);
+        setTasks(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setHasLoaded(false);
+    fetchTasks();
+  }, [filter, query, pathname]);
+
+  return (
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+      <Form
+          className={styles.SearchBar}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <Form.Control
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            type="text"
+            className="mr-sm-2"
+            placeholder="Search tasks"
+          />
+        </Form>
+        {hasLoaded ? (
+          <>
+            {tasks.results.length ? (
+              tasks.results.map((tasks) => (
+                <Task key={tasks.id} {...tasks} setTasks={setTasks} />
+              ))
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )}
+      </Col>
+      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+        <p>Popular profiles for desktop</p>
+      </Col>
+    </Row>
+  );
+}
+
+export default TasksListings;
