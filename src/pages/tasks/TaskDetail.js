2d69bb46cda2f4fd09aafe0c3afa0d1eafc4be4d
jkingportfolio <mycodejourney3@gmail.com>
2023-04-01 17:56:00 +0000

    Add: Basis for posting comment on task detail

--- src/pages/tasks/TaskDetail.js
+++ src/pages/tasks/TaskDetail.js
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from "react";
-
-import { Col, Row } from "react-bootstrap";
-
-import { useParams } from "react-router";
-
-// import appStyles from "../../App.module.css";
-
 import { axiosReq } from "../../api/axiosDefaults";
 import Task from "./Task";
+import CommentForm from "../comments/CommentForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Col, Row, Container } from "react-bootstrap";
+import { useParams } from "react-router";
+import appStyles from "../../App.module.css";
 
 function TaskDetail() {
   const { id } = useParams();
   const [task, setTask] = useState({ results: [] });
+
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
@@ -32,8 +34,21 @@ function TaskDetail() {
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-      <Task {...task.results[0]} setTasks={setTask} taskDetail />
+        <Task {...task.results[0]} setTasks={setTask} taskDetail />
       </Col>
+      <Container className={appStyles.Content}>
+        {currentUser ? (
+          <CommentForm
+            profile_id={currentUser.profile_id}
+            profileImage={profile_image}
+            ptask={id}
+            setTask={setTask}
+            setComments={setComments}
+          />
+        ) : comments.results.length ? (
+          "Comments"
+        ) : null}
+      </Container>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
         Side bar to display other information
       </Col>
