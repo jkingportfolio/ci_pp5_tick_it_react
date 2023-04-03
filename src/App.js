77db10490a3c90fe123b1a142dcfc48d99f60d01
jkingportfolio <mycodejourney3@gmail.com>
2023-04-03 17:20:16 +0000

    Add: ProfilePage files and path to app.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import TaskCreateForm from "./pages/tasks/TaskCreateForm";
 import TaskDetail from "./pages/tasks/TaskDetail";
 import TasksListings from "./pages/tasks/TasksListings";
 import TaskEditForm from "./pages/tasks/TaskEditForm";
+import ProfilePage from "./pages/profiles/ProfilePage";
 
 function App() {
   const currentUser = useCurrentUser();
@@ -44,6 +45,7 @@ function App() {
           <Route exact path="/tasks/create" render={() => <TaskCreateForm />} />
           <Route exact path="/tasks/:id" render={() => <TaskDetail />} />
           <Route exact path="/tasks/:id/edit" render={() => <TaskEditForm />} />
+          <Route exact path="/profile/:id/" render={() => <ProfilePage />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
