322e44c12d9e67bd4b51fe30ef9c904dc099682f
jkingportfolio <mycodejourney3@gmail.com>
2023-04-09 22:21:45 +0000

    Add: Change password button to profile

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -109,6 +109,17 @@ function ProfilePage() {
                 Create Pack
               </Button>
             </div>
+            <div
+              className={`${styles.Border} ${styles.DivFlexChild} ${styles.ButtonContainer} ${styles.TopMargin}`}
+            >
+              <Button
+                className={`${appStyles.Button}`}
+                as={Link}
+                to={`/profiles/${id}/edit/password`}
+              >
+                Change Password
+              </Button>
+            </div>
           </div>
         </Col>
         {/* <Col lg={8}>
