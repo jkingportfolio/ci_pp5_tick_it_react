453add41d5ebe3ce215e94e62bb9c78a30b50ccb
jkingportfolio <mycodejourney3@gmail.com>
2023-04-09 19:37:43 +0000

    Style: Dashboard page

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -10,66 +10,78 @@ import TaskStatusTable from "../../components/TaskStatusTable";
 import TaskCompleteFilter from "../../components/TaskCompleteFilter";
 import ProfilesList from "../../components/ProfilesList";
 
-
-
 const Dashboard = () => {
- 
   const currentUser = useCurrentUser();
 
-//   return (
-//     <>
-//     <Container>
-    
-//         <Row>
-//         <Col md={8} className={`${appStyles.LightText} ${styles.BorderBox}`}>
-//           <Row>
-//           <Col md={12} className={`${appStyles.LightText} ${styles.BorderBox} ${appStyles.Header}`}>
-//          Hello {currentUser.username} {currentUser.image}
-//         </Col>
-//           </Row>
-//           <Row>
-//             <Col md={6} className={`${appStyles.DarkText} ${styles.BorderBox}`}>
-//               <ProfilesList />
-//             </Col>
-//             <Col md={6} className={`${appStyles.LightText} ${styles.BorderBox}`}>
-//               <TaskStatusTable />
-//               {/* <TaskCompleteFilter /> */}
-//             </Col>
-//           </Row>
-//         </Col>
-//         <Col md={4} className={`${appStyles.DarkText} ${styles.BorderBox}`}>
-//         <FreeProfiles />
-//         </Col>
-//       </Row>
-//       </Container>
-//     </>
-//   );
-// };
+  //   return (
+  //     <>
+  //     <Container>
 
-return (
-  <>
-  <Container>  
-      <Row>
-      <Col className={`${appStyles.LightText} ${styles.BorderBox}`}>
-        <Col md={12} className={`${appStyles.LightText} ${styles.BorderBox} ${appStyles.Header}`}>
-       Hello {currentUser.username} {currentUser.image}
-      </Col>
-          <Col md={12} className={`${appStyles.DarkText} ${styles.BorderBox}`}>
-            <ProfilesList />
-          </Col>
-          <Col md={12} className={`${appStyles.LightText} ${styles.BorderBox}`}>
-            <TaskStatusTable />
-            {/* <TaskCompleteFilter /> */}
-          </Col>
-          <Col md={12} className={`${appStyles.DarkText} ${styles.BorderBox}`}>
-      <FreeProfiles />
-      </Col>
-      </Col>
+  //         <Row>
+  //         <Col md={8} className={`${appStyles.LightText} ${styles.BorderBox}`}>
+  //           <Row>
+  //           <Col md={12} className={`${appStyles.LightText} ${styles.BorderBox} ${appStyles.Header}`}>
+  //          Hello {currentUser.username} {currentUser.image}
+  //         </Col>
+  //           </Row>
+  //           <Row>
+  //             <Col md={6} className={`${appStyles.DarkText} ${styles.BorderBox}`}>
+  //               <ProfilesList />
+  //             </Col>
+  //             <Col md={6} className={`${appStyles.LightText} ${styles.BorderBox}`}>
+  //               <TaskStatusTable />
+  //               {/* <TaskCompleteFilter /> */}
+  //             </Col>
+  //           </Row>
+  //         </Col>
+  //         <Col md={4} className={`${appStyles.DarkText} ${styles.BorderBox}`}>
+  //         <FreeProfiles />
+  //         </Col>
+  //       </Row>
+  //       </Container>
+  //     </>
+  //   );
+  // };
 
-    </Row>
-    </Container>
-  </>
-);
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col className={`${appStyles.LightText} ${styles.BorderBox}`}>
+            <Col
+              md={12}
+              className={`${appStyles.LightText} ${styles.BorderBox} ${appStyles.Header}`}
+            >
+              Hello {currentUser.username} {currentUser.image}
+              <hr className={styles.HorizontalLine} />
+            </Col>
+            <Col
+              md={12}
+              className={`${appStyles.LightText} ${styles.BorderBox}`}
+            >
+              <TaskStatusTable />
+              <hr className={styles.HorizontalLine} />
+            </Col>
+            <Col
+              md={12}
+              className={`${appStyles.DarkText} ${styles.BorderBox}`}
+            >
+              <ProfilesList />
+              <hr className={styles.HorizontalLine} />
+            </Col>
+
+            <Col
+              md={12}
+              className={`${appStyles.DarkText} ${styles.BorderBox}`}
+            >
+              <TaskCompleteFilter />
+              <hr className={styles.HorizontalLine}/>
+            </Col>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default Dashboard;
